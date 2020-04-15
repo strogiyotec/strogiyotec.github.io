3b5e9e53c372fb427bb9f36480decff8d88f115e
strogiyotec <almas337519@gmail.com>
2020-04-14 17:42:11 -0700

    modify about page

---
+++ js/updated_at.js
@@ -0,0 +1,16 @@
+const span = document.getElementById("lastupdated")
+const github_api_url = "https://api.github.com/repos/strogiyotec/strogiyotec.github.io/commits?sha=master"
+fetch(github_api_url)
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data[0])
+        const lastUpdated = new Date(data[0].commit.committer.date);
+        span.textContent += lastUpdated.getDate() + "/" + (lastUpdated.getMonth() + 1) + "/" + lastUpdated.getFullYear();
+
+    })
+    .catch(() => {
+        const lastUpdated = new Date();
+        span.textContent += lastUpdated.getDate() + "/" + (lastUpdated.getMonth() + 1) + "/" + lastUpdated.getFullYear();
+    })
